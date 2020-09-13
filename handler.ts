fe716b0d18d2d2e0fb49150973e73f344a35f459
Dan O'Brien <dobrien@launchdarkly.com>
2020-09-13 16:04:02 -0400

    initial commit

---
+++ handler.ts
@@ -0,0 +1,36 @@
+import * as LaunchDarkly from 'launchdarkly-node-server-sdk'
+
+export const handler = async (): Promise<any> => {
+    // TODO : Enter your LaunchDarkly SDK key here
+    const ldclient = LaunchDarkly.init("YOUR_SDK_KEY");
+
+    const user = {
+    "firstName":"Bob",
+    "lastName":"Loblaw",
+    "key":"bob@example.com",
+    "custom":{
+        "groups":"beta_testers"
+    }
+    };
+
+    ldclient.once('ready', function() {
+    // TODO : Enter the key for your feature flag here
+    ldclient.variation("YOUR_FEATURE_FLAG_KEY", user, false, function(err, showFeature) {
+        if (showFeature) {
+        // application code to show the feature
+        console.log("Showing your feature to " + user.key );
+        } else {
+        // the code to run if the feature is off
+        console.log("Not showing your feature to " + user.key);
+        }
+
+        // Close the LaunchDarkly SDK to flush all buffered events and close all open connections.
+        //
+        // IMPORTANT: in a real application, this step is something you would only do when the application is
+        // about to quit-- NOT after every call to variation(). The reason that this step is inside the variation
+        // handler is flags cannot be evaluated after the SDK is closed.
+        ldclient.close();
+    });
+    });
+    return {};
+  }
